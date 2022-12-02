25c03f52f787939a62143cc7cdfa0337477c83d5
yang <89863442+yang-0201@users.noreply.github.com>
2022-12-03 00:00:49 +0800

    Add files via upload

---
+++ configs/model_yaml/yolov6l6_p7_yaml.py
@@ -0,0 +1,50 @@
+# YOLOv6l model
+model = dict(
+    type='YOLOv6l_yaml',
+    pretrained="weights/yolov6l6_p7_yaml.pt",
+    build_type = 'yaml',
+    yaml_file = 'configs/yaml/yolov6l6_p7.yaml',
+    depth_multiple=1.0,
+    width_multiple=1.0,
+    head=dict(
+        type='EffiDeHead',
+        num_layers=5,
+        anchors=1,
+        strides=[8, 16, 32, 64, 128],
+        iou_type='giou',
+        use_dfl=True,
+        reg_max=16, #if use_dfl is False, please set reg_max to 0
+        distill_weight={
+            'class': 2.0,
+            'dfl': 1.0,
+        },
+    )
+)
+
+solver = dict(
+    optim='SGD',
+    lr_scheduler='Cosine',
+    lr0=0.001,
+    lrf=0.012,
+    momentum=0.843,
+    weight_decay=0.00036,
+    warmup_epochs=2.0,
+    warmup_momentum=0.5,
+    warmup_bias_lr=0.05
+)
+
+data_aug = dict(
+    hsv_h=0.0138,
+    hsv_s=0.664,
+    hsv_v=0.464,
+    degrees=0.373,
+    translate=0.245,
+    scale=0.898,
+    shear=0.602,
+    flipud=0.00856,
+    fliplr=0.5,
+    mosaic=1.0,
+    mixup=0.243,
+)
+training_mode = "conv_silu"
+# use normal conv to speed up training and further improve accuracy.
